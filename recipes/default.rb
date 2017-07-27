95251c302266b31846b5aa63e3ebc207e9b50f58
dp.websphere <dp.websphere@gmail.com>
2017-07-27 10:43:08 -0500

    first commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,51 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+#execute "sudo yum install java-1.7.0-openjdk-devel"
+package 'java-1.7.0-openjdk-devel'
+
+#execute 'sudo groupadd tomcat'
+group 'tomcat'
+
+user 'tomcat' do
+  manage_home false
+  shell '/bin/nologin'
+  group 'tomcat'
+  home '/opt/tomcat'
+end
+
+#wget "http://apache.mirrors.ionfish.org/tomcat/tomcat-8/v8.0.45/bin/apache-tomcat-8.0.45.tar.gz"
+remote_file "apache-tomcat-8.0.45.tar.gz" do
+  source "http://apache.mirrors.ionfish.org/tomcat/tomcat-8/v8.0.45/bin/apache-tomcat-8.0.45.tar.gz"
+end
+
+directory '/opt/tomcat/' do
+  owner 'tomcat'
+  group 'tomcat'
+  mode '0755'
+  recursive true
+end
+
+# TODO: not desired state
+execute 'tar xvf apache-tomcat-8.0.45.tar.gz -C /opt/tomcat --strip-components=1'
+
+execute "chown" do
+  command "chown -R tomcat:tomcat /opt/tomcat/*"
+  only_if 'ls /opt/tomcat/conf', :owner => 'root'
+end
+
+execute "chmod" do
+  command "chmod -R 0755 /opt/tomcat/*"
+  only_if 'ls /opt/tomcat/conf', :owner => 'tomcat'
+end
+
+template '/etc/systemd/system/tomcat.service' do
+  source 'tomcat.service.erb'
+end
+
+execute 'systemctl daemon-reload'
+
+service 'tomcat' do
+  action [:start, :enable]
+end
--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -4,7 +4,7 @@
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
-
+=begin
 unless os.windows?
   # This is an example test, replace with your own test.
   describe user('root'), :skip do
@@ -16,3 +16,77 @@ end
 describe port(80), :skip do
   it { should_not be_listening }
 end
+=end
+#require 'spec_helper'
+
+describe command("curl http://localhost:8080") do
+  its(:stdout) { should match /Tomcat/ }
+end
+
+describe package ('java-1.7.0-openjdk-devel') do
+ it {should be_installed}
+end
+
+describe group('tomcat') do
+  it { should exist }
+end
+
+describe user('tomcat') do
+  it { should exist }
+  it { should belong_to_group 'tomcat' }
+  it { should have_home_directory '/opt/tomcat' }
+end
+
+describe file('/opt/tomcat') do
+  it { should exist }
+  it { should be_directory }
+  it {should be_owned_by 'tomcat'}
+end
+
+#checking the permissions on webapps,work,temp,logs directories
+%w[ conf webapps work temp logs].each do |path|
+  describe file("/opt/tomcat/#{path}") do
+    it { should exist }
+    it { should be_mode 0755 }
+    it { should be_owned_by 'tomcat'}
+  end
+end
+
+=begin
+describe file('/opt/tomcat/conf') do
+  it { should exist }
+  it { should be_mode 0755 }
+  it {should be_owned_by 'tomcat'}
+end
+
+
+describe file('/opt/tomcat/conf') do
+  it { should exist }
+  it { should be_mode 755 }
+  it {should be_owned_by 'tomcat'}
+end
+
+describe file('/opt/tomcat/webapps') do
+  it { should exist }
+  it { should be_mode 755 }
+  it {should be_owned_by 'tomcat'}
+end
+
+describe file('/opt/tomcat/work') do
+  it { should exist }
+  it { should be_mode 755 }
+  it {should be_owned_by 'tomcat'}
+end
+
+describe file('/opt/tomcat/temp') do
+  it { should exist }
+  it { should be_mode 755 }
+  it {should be_owned_by 'tomcat'}
+end
+
+describe file('/opt/tomcat/logs') do
+  it { should exist }
+  it { should be_mode 755 }
+  it {should be_owned_by 'tomcat'}
+end
+=end
